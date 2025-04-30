b6d60079655c083fdc22e2ce61aa6c607152988c
SinghAbhishek1998 <abhisheksja1998@gmail.com>
2025-04-30 17:37:32 +0530

    added Register controller and some fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,44 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import { User } from "../models/user.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser =  asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message:"ok"
+    const {email, username,password, fullName} = req.body;
+    if([email,username,password,fullName].some((field)=> field?.trim()==="")){
+        throw new ApiError(400,"All fields are required!")
+    }
+    const existedUser = User.findOne({
+        $or:[{username}, {email}]
     })
+    if (existedUser) throw new ApiError(409,"User already exists")
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is required")
+    }
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    if(!avatar){
+        throw new ApiError(400,"Avatar file is required")
+    }
+    const user = await User.create({
+        fullname:fullName,
+        avatar:avatar.url,
+        coverImage:coverImage?.url || "",
+        email,
+        password,
+        username:username.toLowerCase()
+
+    })
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    if(!createdUser){
+        throw new ApiError(500,"Something went wrong with user registration!!")
+    }
+    return res.status(201).json(new ApiResponse(200,"User Registered Successfully",createdUser))
 })
 
 export {registerUser}
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -7,5 +7,5 @@ class ApiResponse {
     }
   }
   
-  module.exports = ApiResponse;
+ export {ApiResponse}
   
